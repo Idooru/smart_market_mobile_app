1f326a40b25c53c0cad97952cf6cd24506779e84
idooru <shere1765@gmail.com>
2025-04-18 17:32:24 +0900

    [etc] 임시 데이터

---
+++ lib/model/main/presentation/page/test2.page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Test2Widget extends StatelessWidget {
+  const Test2Widget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
