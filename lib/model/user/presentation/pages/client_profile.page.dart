fdad8040f30f44fec6c47e2258df6d70dd783007
idooru <shere1765@gmail.com>
2025-05-13 17:30:54 +0900

    [etc] 임시 페이지 생성

---
+++ lib/model/user/presentation/pages/client_profile.page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ClientProfilePage extends StatefulWidget {
+  const ClientProfilePage({super.key});
+
+  @override
+  State<ClientProfilePage> createState() => _ClientProfilePageState();
+}
+
+class _ClientProfilePageState extends State<ClientProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Hi!"),
+    );
+  }
+}
