ab0ca6d17747712e681e0635e208f82531a216dc
idooru <shere1765@gmail.com>
2025-05-29 12:32:46 +0900

    [feature] 길게 누를 시 장바구니 메뉴 표시

--- lib/model/cart/presentation/widgets/cart_item.widget.dart
+++ lib/model/cart/presentation/widgets/cart_item.widget.dart
@@ -120,109 +120,112 @@ class _CartItemWidgetState extends State<CartItemWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 100,
-      margin: const EdgeInsets.only(bottom: 10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: const Color.fromARGB(255, 245, 245, 245),
-      ),
-      child: Stack(
-        children: [
-          Container(
-            width: 80,
-            height: 80,
-            margin: const EdgeInsets.all(10),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.network(
-                widget.cart.product.imageUrls[0],
-                fit: BoxFit.fill,
+    return GestureDetector(
+      onLongPress: pressTrailingIcon,
+      child: Container(
+        width: double.infinity,
+        height: 100,
+        margin: const EdgeInsets.only(bottom: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: const Color.fromARGB(255, 245, 245, 245),
+        ),
+        child: Stack(
+          children: [
+            Container(
+              width: 80,
+              height: 80,
+              margin: const EdgeInsets.all(10),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  widget.cart.product.imageUrls[0],
+                  fit: BoxFit.fill,
+                ),
               ),
             ),
-          ),
-          Positioned(
-            top: 10,
-            left: 100,
-            child: SizedBox(
-              height: 80,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SizedBox(
-                    width: 160,
-                    child: Text(
-                      widget.cart.product.name,
-                      style: const TextStyle(
-                        fontSize: 20,
-                        color: Color.fromARGB(255, 50, 50, 50),
-                        overflow: TextOverflow.ellipsis,
-                        fontWeight: FontWeight.w300,
+            Positioned(
+              top: 10,
+              left: 100,
+              child: SizedBox(
+                height: 80,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: 160,
+                      child: Text(
+                        widget.cart.product.name,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          color: Color.fromARGB(255, 50, 50, 50),
+                          overflow: TextOverflow.ellipsis,
+                          fontWeight: FontWeight.w300,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 188,
-                    child: Row(
-                      children: [
-                        SizedBox(
-                          child: Text(
-                            "${NumberFormat('#,###').format(widget.cart.product.price)}원",
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color: Color.fromARGB(255, 100, 100, 100),
-                              overflow: TextOverflow.ellipsis,
-                              fontWeight: FontWeight.w500,
+                    SizedBox(
+                      width: 188,
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            child: Text(
+                              "${NumberFormat('#,###').format(widget.cart.product.price)}원",
+                              style: const TextStyle(
+                                fontSize: 16,
+                                color: Color.fromARGB(255, 100, 100, 100),
+                                overflow: TextOverflow.ellipsis,
+                                fontWeight: FontWeight.w500,
+                              ),
                             ),
                           ),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 2),
-                          child: Icon(
-                            Icons.close,
-                            size: 15,
-                            color: Color.fromARGB(255, 100, 100, 100),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 2),
+                            child: Icon(
+                              Icons.close,
+                              size: 15,
+                              color: Color.fromARGB(255, 100, 100, 100),
+                            ),
                           ),
-                        ),
-                        Text(
-                          "수량: ${widget.cart.quantity}",
-                          style: const TextStyle(
-                            fontSize: 14,
-                            color: Color.fromARGB(255, 100, 100, 100),
-                            fontWeight: FontWeight.w500,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    width: 140,
-                    child: Text(
-                      "합계: ${NumberFormat('#,###').format(widget.cart.totalPrice)}원",
-                      style: const TextStyle(
-                        fontSize: 17,
-                        color: Colors.black,
-                        overflow: TextOverflow.ellipsis,
-                        fontWeight: FontWeight.w500,
+                          Text(
+                            "수량: ${widget.cart.quantity}",
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Color.fromARGB(255, 100, 100, 100),
+                              fontWeight: FontWeight.w500,
+                            ),
+                          )
+                        ],
                       ),
                     ),
-                  )
-                ],
+                    SizedBox(
+                      width: 140,
+                      child: Text(
+                        "합계: ${NumberFormat('#,###').format(widget.cart.totalPrice)}원",
+                        style: const TextStyle(
+                          fontSize: 17,
+                          color: Colors.black,
+                          overflow: TextOverflow.ellipsis,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          Positioned(
-            top: -5,
-            right: -5,
-            child: IconButton(
-              constraints: const BoxConstraints(), // 크기 최소화
-              icon: const Icon(Icons.more_vert, size: 18),
-              onPressed: pressTrailingIcon,
+            Positioned(
+              top: -5,
+              right: -5,
+              child: IconButton(
+                constraints: const BoxConstraints(), // 크기 최소화
+                icon: const Icon(Icons.more_vert, size: 18),
+                onPressed: pressTrailingIcon,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
