9b8d85defd402779fbffc05dee1367d14f70a00a
idooru <shere1765@gmail.com>
2025-05-30 18:37:09 +0900

    [feature] 길게 누를 계좌 메뉴 표시

--- lib/model/account/presentation/widgets/account_item.widget.dart
+++ lib/model/account/presentation/widgets/account_item.widget.dart
@@ -163,64 +163,67 @@ class _AccountItemWidgetState extends State<AccountItemWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 95,
-      margin: const EdgeInsets.only(bottom: 10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: const Color.fromARGB(255, 245, 245, 245),
-      ),
-      child: Stack(
-        children: [
-          Positioned(
-            top: 10,
-            left: 10,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "${widget.account.bank} ${widget.account.accountNumber}",
-                  style: const TextStyle(
-                    fontSize: 14,
-                    color: Color.fromARGB(255, 80, 80, 80),
+    return GestureDetector(
+      onLongPress: pressTrailingIcon,
+      child: Container(
+        width: double.infinity,
+        height: 95,
+        margin: const EdgeInsets.only(bottom: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: const Color.fromARGB(255, 245, 245, 245),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 10,
+              left: 10,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "${widget.account.bank} ${widget.account.accountNumber}",
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Color.fromARGB(255, 80, 80, 80),
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 7),
-                  child: Text(
-                    "${NumberFormat('#,###').format(widget.account.balance)} 원",
-                    style: const TextStyle(fontSize: 24),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 7),
+                    child: Text(
+                      "${NumberFormat('#,###').format(widget.account.balance)} 원",
+                      style: const TextStyle(fontSize: 24),
+                    ),
                   ),
-                ),
-                Text(
-                  "${parseDate(widget.account.createdAt)}에 계좌 등록됨",
-                  style: const TextStyle(
-                    fontSize: 12,
-                    color: Color.fromARGB(255, 120, 120, 120),
-                  ),
-                )
-              ],
+                  Text(
+                    "${parseDate(widget.account.createdAt)}에 계좌 등록됨",
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: Color.fromARGB(255, 120, 120, 120),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          Positioned(
-            top: 10,
-            right: 10,
-            child: Icon(
-              widget.account.isMainAccount ? Icons.check_circle_outline : Icons.circle_outlined,
-              color: widget.account.isMainAccount ? Colors.red : Colors.black,
+            Positioned(
+              top: 10,
+              right: 10,
+              child: Icon(
+                widget.account.isMainAccount ? Icons.check_circle_outline : Icons.circle_outlined,
+                color: widget.account.isMainAccount ? Colors.red : Colors.black,
+              ),
             ),
-          ),
-          Positioned(
-            right: -2,
-            bottom: -2,
-            child: IconButton(
-              constraints: const BoxConstraints(), // 크기 최소화
-              icon: const Icon(Icons.more_vert, size: 15),
-              onPressed: pressTrailingIcon,
+            Positioned(
+              right: -2,
+              bottom: -2,
+              child: IconButton(
+                constraints: const BoxConstraints(), // 크기 최소화
+                icon: const Icon(Icons.more_vert, size: 15),
+                onPressed: pressTrailingIcon,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
