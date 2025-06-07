0ceee10fb76c5ca9ed7ec1b2cf87e8b2d028363d
idooru <shere1765@gmail.com>
2025-06-07 16:25:17 +0900

    [etc] 상품 상세 페이지 UI 재구성

--- lib/model/product/presentation/pages/detail_product.page.dart
+++ lib/model/product/presentation/pages/detail_product.page.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:smart_market/core/errors/dio_fail.error.dart';
-import 'package:smart_market/core/utils/format_number.dart';
 import 'package:smart_market/core/utils/get_it_initializer.dart';
 import 'package:smart_market/core/utils/get_snackbar.dart';
+import 'package:smart_market/core/widgets/common/common_button_bar.widget.dart';
 import 'package:smart_market/core/widgets/handler/loading_handler.widget.dart';
 import 'package:smart_market/model/cart/domain/entities/create_cart.entity.dart';
 import 'package:smart_market/model/cart/presentation/dialog/create_cart.dialog.dart';
 import 'package:smart_market/model/product/domain/entities/detail_product.entity.dart';
 import 'package:smart_market/model/product/domain/service/product.service.dart';
-import 'package:smart_market/model/product/presentation/widgets/display_average_score.widget.dart';
 import 'package:smart_market/model/product/presentation/widgets/image/product_image_grid.widget.dart';
-import 'package:smart_market/model/product/presentation/widgets/item/review_item.widget.dart';
-import 'package:smart_market/model/user/utils/get_user_info.dart';
 
 import '../../../../core/errors/connection_error.dart';
 import '../../../../core/utils/check_jwt_duration.dart';
+import '../../../../core/utils/format_number.dart';
 import '../../../../core/widgets/dialog/handle_network_error_on_dialog.dialog.dart';
 import '../../../../core/widgets/handler/internal_server_error_handler.widget.dart';
 import '../../../../core/widgets/handler/network_error_handler.widget.dart';
 import '../../../cart/domain/service/cart.service.dart';
 import '../../../user/presentation/dialog/invitation_login.dialog.dart';
 import '../../../user/utils/check_is_logined.dart';
+import '../widgets/display_average_score.widget.dart';
+import '../widgets/item/review_item.widget.dart';
 
 class DetailProductPageArgs {
   final String productId;
@@ -83,18 +83,42 @@ class _DetailProductPageState extends State<DetailProductPage> {
     );
   }
 
+  Widget getItemArea(String title, Widget child) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.only(bottom: 15),
+      decoration: BoxDecoration(
+        border: Border.all(width: 0.6, color: const Color.fromARGB(255, 210, 210, 210)),
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 3),
+            child: Text(
+              title,
+              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+            ),
+          ),
+          child
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: const Text("detail"),
+        flexibleSpace: Container(color: const Color.fromARGB(255, 240, 240, 240)),
+        title: const Text("Product Detail"),
       ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: Colors.white,
-        padding: const EdgeInsets.all(20),
+      backgroundColor: const Color.fromARGB(255, 240, 240, 240),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
         child: FutureBuilder(
           future: _getDetailProductFuture,
           builder: (BuildContext context, AsyncSnapshot<ResponseDetailProduct> snapshot) {
@@ -115,151 +139,95 @@ class _DetailProductPageState extends State<DetailProductPage> {
               }
             } else if (snapshot.hasData) {
               final data = snapshot.data!;
-              return SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+              return Scaffold(
+                backgroundColor: const Color.fromARGB(255, 240, 240, 240),
+                body: Column(
                   children: [
-                    const Text(
-                      "상품 이미지",
-                      style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
-                    ),
-                    const SizedBox(height: 5),
-                    ProductImageGridWidget(imageUrls: data.product.imageUrls),
-                    const SizedBox(height: 5),
-                    const Text(
-                      "상품 정보",
-                      style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
-                    ),
-                    const SizedBox(height: 5),
-                    Container(
-                      width: double.infinity,
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("이름: ${data.product.name}", style: const TextStyle(fontSize: 14, color: Color.fromARGB(255, 50, 50, 50))),
-                          Text("가격: ${formatNumber(data.product.price)}원", style: const TextStyle(fontSize: 14, color: Color.fromARGB(255, 50, 50, 50))),
-                          Text("원산지: ${data.product.origin}", style: const TextStyle(fontSize: 14, color: Color.fromARGB(255, 50, 50, 50))),
-                          Text("분류: ${data.product.category}", style: const TextStyle(fontSize: 14, color: Color.fromARGB(255, 50, 50, 50))),
-                          Text("설명: ${data.product.description}", style: const TextStyle(fontSize: 14, color: Color.fromARGB(255, 50, 50, 50))),
-                          Row(
-                            children: [
-                              const Text("평점: ", style: TextStyle(fontSize: 14, color: Color.fromARGB(255, 50, 50, 50))),
-                              DisplayAverageScoreWidget(averageScore: data.product.averageScore),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 15),
-                    const Text(
-                      "상품 리뷰",
-                      style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
-                    ),
-                    const SizedBox(height: 5),
-                    Container(
-                      width: double.infinity,
-                      height: data.reviews.isEmpty ? 70 : 450,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: (() => data.reviews.isEmpty
-                          ? const Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(Icons.warning, size: 18),
-                                SizedBox(width: 5),
-                                Text("아직 상품 리뷰가 존재하지 않습니다."),
-                              ],
-                            )
-                          : SingleChildScrollView(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: data.reviews.asMap().entries.map((entry) {
-                                  int index = entry.key;
-                                  Review review = entry.value;
-                                  EdgeInsets margin = index != data.reviews.length - 1 ? const EdgeInsets.fromLTRB(8, 8, 8, 0) : const EdgeInsets.fromLTRB(8, 8, 8, 8);
-                                  return ReviewItemWidget(review: review, margin: margin);
-                                }).toList(),
-                              ),
-                            ))(),
-                    ),
-                    getUserInfo().userRole != "admin"
-                        ? Row(
-                            children: [
-                              Flexible(
-                                flex: 1,
-                                child: GestureDetector(
-                                  onTap: () {},
-                                  child: Container(
-                                    margin: const EdgeInsets.all(10),
-                                    height: 60,
-                                    decoration: BoxDecoration(
-                                      color: const Color.fromARGB(255, 200, 200, 200),
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                    child: const Row(
-                                      mainAxisAlignment: MainAxisAlignment.center,
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            getItemArea("상품 이미지", ProductImageGridWidget(imageUrls: data.product.imageUrls)),
+                            getItemArea(
+                              "상품 정보",
+                              Padding(
+                                padding: const EdgeInsets.only(left: 10, top: 10),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("이름: ${data.product.name}", style: const TextStyle(fontSize: 14, color: Color.fromARGB(255, 50, 50, 50))),
+                                    Text("가격: ${formatNumber(data.product.price)}원", style: const TextStyle(fontSize: 14, color: Color.fromARGB(255, 50, 50, 50))),
+                                    Text("원산지: ${data.product.origin}", style: const TextStyle(fontSize: 14, color: Color.fromARGB(255, 50, 50, 50))),
+                                    Text("분류: ${data.product.category}", style: const TextStyle(fontSize: 14, color: Color.fromARGB(255, 50, 50, 50))),
+                                    Text("설명: ${data.product.description}", style: const TextStyle(fontSize: 14, color: Color.fromARGB(255, 50, 50, 50))),
+                                    Row(
                                       children: [
-                                        Icon(
-                                          Icons.edit,
-                                          size: 19,
-                                          color: Color.fromARGB(255, 70, 70, 70),
-                                        ),
-                                        SizedBox(
-                                          width: 5,
-                                        ),
-                                        Text(
-                                          "리뷰 작성하기",
-                                          style: TextStyle(
-                                            color: Color.fromARGB(255, 70, 70, 70),
-                                            fontSize: 17,
-                                          ),
-                                        )
+                                        const Text("평점: ", style: TextStyle(fontSize: 14, color: Color.fromARGB(255, 50, 50, 50))),
+                                        DisplayAverageScoreWidget(averageScore: data.product.averageScore),
                                       ],
                                     ),
-                                  ),
+                                  ],
                                 ),
                               ),
-                              Flexible(
-                                flex: 1,
-                                child: GestureDetector(
-                                  onTap: () => pressCreateCart(data),
-                                  child: Container(
-                                    margin: const EdgeInsets.all(10),
-                                    height: 60,
-                                    decoration: BoxDecoration(
-                                      color: Colors.blue,
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                    child: const Row(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: [
-                                        Icon(
-                                          Icons.shopping_cart,
-                                          size: 19,
-                                          color: Colors.white,
+                            ),
+                            getItemArea(
+                              "상품 리뷰",
+                              SizedBox(
+                                height: data.reviews.isEmpty ? 70 : 450,
+                                child: data.reviews.isEmpty
+                                    ? const Row(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                          Icon(Icons.warning, size: 18),
+                                          SizedBox(width: 5),
+                                          Text("아직 상품 리뷰가 존재하지 않습니다."),
+                                        ],
+                                      )
+                                    : SingleChildScrollView(
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: data.reviews.asMap().entries.map((entry) {
+                                            int index = entry.key;
+                                            Review review = entry.value;
+                                            EdgeInsets margin = index != data.reviews.length - 1 ? const EdgeInsets.fromLTRB(8, 8, 8, 0) : const EdgeInsets.fromLTRB(8, 8, 8, 8);
+                                            return ReviewItemWidget(review: review, margin: margin);
+                                          }).toList(),
                                         ),
-                                        SizedBox(
-                                          width: 5,
-                                        ),
-                                        Text(
-                                          "장바구니 담기",
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 17,
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ],
-                          )
-                        : const SizedBox.shrink(),
-                    const SizedBox(height: 5),
+                                      ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                   ],
+                ),
+                bottomNavigationBar: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: Row(
+                    children: [
+                      Flexible(
+                        flex: 1,
+                        child: CommonButtonBarWidget(
+                          icon: Icons.shopping_cart,
+                          title: "장바구니 담기",
+                          backgroundColor: Colors.blue,
+                          pressCallback: () => pressCreateCart(data),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Flexible(
+                        flex: 1,
+                        child: CommonButtonBarWidget(
+                          icon: Icons.edit,
+                          title: "리뷰 작성하기",
+                          backgroundColor: const Color.fromARGB(255, 200, 200, 200),
+                          pressCallback: () {},
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               );
             } else {
