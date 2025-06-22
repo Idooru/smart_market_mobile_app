8dadfefc0c626b10176a8d000fbcb91b9dacfdd2
idooru <shere1765@gmail.com>
2025-06-22 13:48:50 +0900

    [bug fix] 상품 상세 페이지에서 넘어오지 않은 경우 backRoute가 null 이어서 발생한 문제 해결

--- lib/model/order/presentation/pages/create_order.page.dart
+++ lib/model/order/presentation/pages/create_order.page.dart
@@ -3,7 +3,6 @@ import 'package:provider/provider.dart';
 import 'package:smart_market/core/themes/theme_data.dart';
 import 'package:smart_market/core/widgets/common/conditional_button_bar.widget.dart';
 import 'package:smart_market/model/account/domain/entities/account.entity.dart';
-import 'package:smart_market/model/cart/domain/service/cart.service.dart';
 import 'package:smart_market/model/order/presentation/pages/complete_create_order.page.dart';
 import 'package:smart_market/model/order/presentation/provider/create_order.provider.dart';
 import 'package:smart_market/model/order/presentation/widgets/calculate_price.widget.dart';
@@ -12,6 +11,7 @@ import 'package:smart_market/model/user/presentation/provider/edit_user_column.p
 
 import '../../../../core/utils/get_it_initializer.dart';
 import '../../../cart/domain/entities/cart.entity.dart';
+import '../../../cart/domain/service/cart.service.dart';
 import '../../../user/presentation/widgets/edit/edit_address.widget.dart';
 import '../../domain/entities/create_order.entity.dart';
 
@@ -94,12 +94,22 @@ class _CreateOrderPageState extends State<CreateOrderPage> {
             flexibleSpace: appBarColor,
             leading: IconButton(
               onPressed: () {
-                if (widget.backRoute!.isNotEmpty) {
-                  final CartService cartService = locator<CartService>();
+                /* 상품 상세 페이지에서 즉시 구매로 넘어온 경우 이전 페이지로 뒤돌아갈 backRoute가 담김
+                 * 이를 조건으로 상품 상세 페이지에서 담긴 상품인지, 장바구니 리스트 페이지에서 담긴 상품인지 구별함
+                 * 즉시 구매로 장바구니에 담은 상품이 이 페이지를 나가서 존재할 필요가 없으므로
+                 * 즉시 구매로 장바구니에 담은 상품을 제거함
+                 */
+                if (widget.backRoute != null) {
+                  CartService cartService = locator<CartService>();
                   for (Cart cart in orderProvider.carts) {
                     cartService.deleteCart(cart.id);
                   }
                 }
+
+                /* 반대의 경우는 장바구니 리스트 페이지에서 넘어온 경우
+                 * 사용자가 구매를 미루려는 의도로 페이지를 나갈 수 있으니
+                 * 굳이 장바구니에 담은 상품을 제거 할 필요 없음
+                 */
                 Navigator.of(context).pop();
               },
               icon: const Icon(Icons.arrow_back_ios),
