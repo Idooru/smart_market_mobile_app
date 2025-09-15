c10652741bbeaa6997da6cc9e0ac18b250c7072d
shlee <shere1765@gmail.com>
2025-09-15 21:35:37 +0900

    [bugfix] 에러 메세지를 불러오지 못하는 문제 해결

--- lib/core/common/network_handler.mixin.dart
+++ lib/core/common/network_handler.mixin.dart
@@ -1,19 +1,22 @@
-import 'package:smart_market/core/errors/dio_fail.error.dart';
-
 import '../errors/connection_error.dart';
+import '../errors/dio_fail.error.dart';
 
 mixin NetWorkHandler {
   String branchErrorMessage(Object error) {
-    if (error is ConnectionError) {
-      return "서버와 연결되지 않습니다.";
-    } else if (error is DioFailError) {
-      if (error.response!.data["statusCode"] == 500) {
-        return "서버 내부에서 에러가 발생하였습니다.";
+    try {
+      if (error is ConnectionError) {
+        return "서버와 연결되지 않습니다.";
+      } else if (error is DioFailError) {
+        if (error.response!.data["statusCode"] == 500) {
+          return "서버 내부에서 에러가 발생하였습니다.";
+        } else {
+          return error.response!.data["message"];
+        }
       } else {
-        return error.response!.data["reason"];
+        return "$error";
       }
-    } else {
-      return "$error";
+    } catch (err) {
+      return "";
     }
   }
 }
